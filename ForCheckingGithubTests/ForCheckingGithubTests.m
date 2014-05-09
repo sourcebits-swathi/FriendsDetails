5be5d0e4a719af2db487f4968e54ac3101f21a9d
Sourcebits <swathi.tata@sourcebits.com>
2014-05-09 11:37:28 +0530

    first commit

---
+++ ForCheckingGithubTests/ForCheckingGithubTests.m
@@ -0,0 +1,34 @@
+//
+//  ForCheckingGithubTests.m
+//  ForCheckingGithubTests
+//
+//  Created by Swathi Tata on 09/05/14.
+//  Copyright (c) 2014 Swathi Tata. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ForCheckingGithubTests : XCTestCase
+
+@end
+
+@implementation ForCheckingGithubTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
